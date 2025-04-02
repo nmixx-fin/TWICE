f41984639f208509b383a23e2ad01be323dd9518
yeowonh <hwyewon@gmail.com>
2025-04-03 04:53:06 +0900

    bugfix : Modify sts code files

--- finance_mteb/tasks/STS/kor/KorFinSTS.py
+++ finance_mteb/tasks/STS/kor/KorFinSTS.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 from finance_mteb.abstasks.TaskMetadata import TaskMetadata
 from ....abstasks import AbsTaskSTS
+from ....evaluation.evaluators import STSEvaluator
+from ....MTEBResults import ScoresDict
+import numpy as np
 
 class KorFinSTS(AbsTaskSTS):
     metadata = TaskMetadata(
@@ -31,6 +34,20 @@ class KorFinSTS(AbsTaskSTS):
             return example
             
         self.dataset = self.dataset.map(validate_example)
+        
+        # score 값들의 고유값 확인
+        unique_scores = np.unique(self.dataset['score'])
+        
+        # 0과 1만 있는지 확인 (float나 int 모두 고려)
+        is_binary = all(np.isclose(score, 0) or np.isclose(score, 1) for score in unique_scores)
+        
+        if is_binary:
+            # 이진 분류인 경우 int로 변환
+            self.dataset = self.dataset.map(lambda x: {**x, 'score': int(x['score'])}, remove_columns=['score'])
+            self.is_binary = True
+        else:
+            # 이진 분류가 아닌 경우 float 유지
+            self.is_binary = False
 
     @property
     def metadata_dict(self) -> dict[str, str]:
@@ -41,3 +58,30 @@ class KorFinSTS(AbsTaskSTS):
             "text_columns": ["sentence1", "sentence2"]
         })
         return metadata_dict
+
+    def _evaluate_subset(self, model, data_split, **kwargs) -> ScoresDict:
+        if self.is_binary:
+            # 이진 분류인 경우 정규화 없이 원래 값 사용
+            evaluator = STSEvaluator(
+                data_split["sentence1"],
+                data_split["sentence2"],
+                data_split["score"],
+                task_name=self.metadata.name,
+                **kwargs,
+            )
+        else:
+            # 이진 분류가 아닌 경우 정규화 수행
+            def normalize(x):
+                return (x - self.min_score) / (self.max_score - self.min_score)
+            normalized_scores = list(map(normalize, data_split["score"]))
+            evaluator = STSEvaluator(
+                data_split["sentence1"],
+                data_split["sentence2"],
+                normalized_scores,
+                task_name=self.metadata.name,
+                **kwargs,
+            )
+        
+        scores = evaluator(model)
+        self._add_main_score(scores)
+        return scores
