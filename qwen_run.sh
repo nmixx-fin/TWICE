200675454b330489195e36b04bcb2fe1cf8d776f
yeowonh <hwyewon@gmail.com>
2025-04-03 02:54:28 +0000

    Add STS result files

---
+++ qwen_run.sh
@@ -0,0 +1,7 @@
+python eval_KorFinMTEB.py --model_name_or_path Alibaba-NLP/gte-Qwen2-1.5B-instruct --task_type CLASSIFICATION
+python eval_KorFinMTEB.py --model_name_or_path Alibaba-NLP/gte-Qwen2-1.5B-instruct --task_type CLUSTERING
+python eval_KorFinMTEB.py --model_name_or_path Alibaba-NLP/gte-Qwen2-1.5B-instruct --task_type PAIRCLASSIFICATION
+python eval_KorFinMTEB.py --model_name_or_path Alibaba-NLP/gte-Qwen2-1.5B-instruct --task_type RERANKING
+python eval_KorFinMTEB.py --model_name_or_path Alibaba-NLP/gte-Qwen2-1.5B-instruct --task_type RETRIEVAL
+python eval_KorFinMTEB.py --model_name_or_path Alibaba-NLP/gte-Qwen2-1.5B-instruct --task_type STS
+python eval_KorFinMTEB.py --model_name_or_path Alibaba-NLP/gte-Qwen2-1.5B-instruct --task_type SUMMARIZATION
