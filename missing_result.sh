6c03fc731d92c0b42d88355c8b62c85208823603
yeowonh <hwyewon@gmail.com>
2025-04-10 03:56:38 +0900

    Add shell files

---
+++ missing_result.sh
@@ -0,0 +1,3 @@
+python eval_KorFinMTEB.py --model_name_or_path BAAI/bge-en-icl --task_type CLASSIFICATION
+python eval_KorFinMTEB.py --model_name_or_path intfloat/e5-mistral-7b-instruct --task_type CLASSIFICATION
+python eval_KorFinMTEB.py --model_name_or_path text-embedding-3-small --task_type CLASSIFICATION
